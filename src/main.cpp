18c048b68dc25753d5064a43ea88f12435cad1e4
lee <lee.hudson1384@yahoo.co.uk>
2021-06-25 20:35:02 +0100

    Got GPS working including sleep

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,17 @@ B5 62 06 57 08 00 01 00 00 00 50 4F 54 53 AC 85
 Wake:
 B5 62 06 57 08 00 01 00 00 00 20 4E 55 52 7B C3
 
+Disable NMEA messages:
+
+B5 62 06 01 03 00 F0 00 00 FA 0F
+B5 62 06 01 03 00 F0 01 00 FB 11
+B5 62 06 01 03 00 F0 02 00 FC 13
+B5 62 06 01 03 00 F0 03 00 FD 15
+B5 62 06 01 03 00 F0 04 00 FE 17
+B5 62 06 01 03 00 F0 05 00 FF 19
+
+Poll POSLLH:
+B5 62 01 02 00 00 03 0A
 
 */
 
@@ -19,13 +30,26 @@ B5 62 06 57 08 00 01 00 00 00 20 4E 55 52 7B C3
 
 #include <SoftwareSerial.h>
 
+#define LOOP_DELAY  1000
+#define GPS_LOCK_MSG_LIMIT  180  
+#define HORIZONTAL_ACC_THRESHOLD  10
+#define STAY_AWAKE_CYCLE 60
+
+
 // Connect the GPS RX/TX to arduino pins 3 and 5
 SoftwareSerial gpsPort = SoftwareSerial(10,11);
 
+const unsigned char gpsConfigCommands[6][11]= {{0xB5, 0x62, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x00, 0x00, 0xFA, 0x0F},
+                                               {0xB5, 0x62, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x01, 0x00, 0xFB, 0x11},
+                                               {0xB5, 0x62, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x02, 0x00, 0xFC, 0x13},
+                                               {0xB5, 0x62, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x03, 0x00, 0xFD, 0x15},
+                                               {0xB5, 0x62, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x04, 0x00, 0xFE, 0x17},
+                                               {0xB5, 0x62, 0x06, 0x01, 0x03, 0x00, 0xF0, 0x05, 0x00, 0xFF, 0x19}};
 const unsigned char gpsSleepCommand[] = {0xB5, 0x62, 0x06, 0x57, 0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x50, 0x4F, 0x54, 0x53, 0xAC, 0x85};
 const unsigned char gpsWakeCommand[] = {0xB5, 0x62, 0x06, 0x57, 0x08, 0x00, 0x01, 0x00, 0x00, 0x00, 0x20, 0x4E, 0x55, 0x52, 0x7B, 0xC3};
-
+const unsigned char gpsPOSLLH_CMD[]={0xB5, 0x62, 0x01, 0x02, 0x00, 0x00, 0x03, 0x0A};
 const unsigned char UBX_HEADER[] = { 0xB5, 0x62 };
+int gpsConfigured,gpsMessageCounter,gpsLock,gpsPositionRequest,gpsAwake,gpsAwakeCounter;
 
 struct NAV_POSLLH {
   unsigned char cls;
@@ -92,48 +116,102 @@ bool processGPS() {
   return false;
 }
 
-void gpsSleep(){
-  gpsPort.write(gpsSleepCommand,sizeof(gpsSleepCommand));
+void gpsWake(){
+  Serial.println("GPS waking up!");
+  gpsPort.write(gpsWakeCommand,sizeof(gpsWakeCommand));
+  gpsAwake = 1;
+  gpsAwakeCounter = 0;
+  delay(500);
+  gpsPort.flush();
+  delay(100);
 }
 
-void gpsWake(){
-  gpsPort.write(gpsWakeCommand,sizeof(gpsWakeCommand));
+void gpsSleep(){
+  Serial.println("GPS going to sleep...");
+  gpsPort.write(gpsSleepCommand,sizeof(gpsSleepCommand));
+  gpsAwake = 0;
+  gpsAwakeCounter = 0;
+  gpsLock = 0;
+  delay(500);
+  gpsPort.flush();
+  delay(100);
 }
+
+
+// Write all configure commands
+void gpsConfigure(){
+  for(int i = 0; i < sizeof(gpsConfigCommands)/sizeof(gpsConfigCommands[0]); i++)
+    gpsPort.write(gpsConfigCommands[i],sizeof(gpsConfigCommands[i]));
+  delay(500);
+  gpsSleep();
+  gpsConfigured = 1;
+}
+
+
 
 void setup() 
 {
   Serial.begin(9600);
   gpsPort.begin(9600);
   Serial.println("Awake");
+  gpsConfigured = 0;
+  gpsMessageCounter = 0;
+  gpsLock = 0;
+  gpsPositionRequest = 0;
+  gpsAwake = 1;
 }
 
 void loop() {
-  
 
+  if (!gpsConfigured){
+    delay(1000);
+    gpsConfigure();
+  }
+    
+
+  
   if(Serial.available()){
     char c = Serial.read();
-    Serial.print("Got: ");
-    Serial.println(c);
-    if(c=='s'){
-      Serial.println("Sleeping...");
-      gpsSleep();
+    if(c=='p'){
+      Serial.println("Got position command, waiting for lock...");
+      gpsPositionRequest = 1;
+    }else{
+      Serial.flush();
     }
-    if(c=='w'){
-      Serial.println("Waking...");
+  }
+
+  // Get GPS position
+  if(gpsPositionRequest){
+    gpsAwakeCounter = 0;
+    if(!gpsAwake)
       gpsWake();
+
+    gpsPort.write(gpsPOSLLH_CMD,sizeof(gpsPOSLLH_CMD));
+    delay(100);
+
+
+    if(processGPS()&&((posllh.hAcc/1000.0f <= HORIZONTAL_ACC_THRESHOLD)||(gpsMessageCounter>GPS_LOCK_MSG_LIMIT))){
+      gpsLock = 1;
+      gpsMessageCounter = 0;
+      gpsPositionRequest = 0;
+      Serial.print("http://maps.google.com/?q=");Serial.print(posllh.lat/10000000.0f,8);Serial.print(",");Serial.print(posllh.lon/10000000.0f,8);Serial.println();
+
+      Serial.print(" hAcc: ");    Serial.print(posllh.hAcc/1000.0f);
+      Serial.print(" vAcc: ");    Serial.print(posllh.vAcc/1000.0f);
+      Serial.println();
     }
 
+    if(!gpsLock)
+      gpsMessageCounter++;
   }
 
+  if(gpsLock && gpsAwake)
+    gpsAwakeCounter++;
 
-  if ( processGPS() ) {
-    Serial.print("http://maps.google.com/?q=");Serial.print(posllh.lat/10000000.0f,8);Serial.print(",");Serial.print(posllh.lon/10000000.0f,8);Serial.println();
+  if(gpsAwakeCounter > STAY_AWAKE_CYCLE)
+    gpsSleep();
 
-    Serial.print(" hAcc: ");    Serial.print(posllh.hAcc/1000.0f);
-    Serial.print(" vAcc: ");    Serial.print(posllh.vAcc/1000.0f);
-    Serial.println();
-  }
   
-    
+  delay(1000);
 
 }
